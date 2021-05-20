3e53d58cbc6577a3040350c05e7c23bdd9c1bee9
Khalid Rizki Ananta <khalidrizki01@mail.ugm.ac.id>
2021-05-21 00:36:16 +0700

    mengembalikan connString jadi NgDBModel aja

--- Notegram/NotegramDBModel.cs
+++ Notegram/NotegramDBModel.cs
@@ -8,7 +8,7 @@ namespace Notegram
     public partial class NotegramDBModel : DbContext
     {
         public NotegramDBModel()
-            : base("name=NotegramDBModel")
+            : base("name=NgDBModel")
         {
         }
 
